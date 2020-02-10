5eda8b748e148e27bb653d682944778e65e0b5ec
hakiso <hakiso.gerson@gmail.com>
2020-02-10 10:17:08 -0300

    Atualização

--- aposentadoria.js
+++ aposentadoria.js
@@ -3,12 +3,14 @@ const sexo = "F";
 const idade = 48;
 const contribuicao = 23;
 
-if (contribuicao >= 30) {
-    if (idade >= 85){
-        console.log('Silvana, você pode se aposentar!');
-    } else {
-        console.log('Silvana, você ainda não pode se aposentar!');
-    };
+const calculcocontribuicao = idade + contribuicao
+
+const homemPodeAposentar = sexo == 'M' && contribuicao >= 35 && calculcocontribuicao >= 95
+const mulherPodeAposentar = sexo == 'F' && contribuicao >= 30 && calculcocontribuicao >= 85
+
+if ( homemPodeAposentar || mulherPodeAposentar ) {
+    console .log('Silvana, você pode se aposentar!')
+
 } else {
-    console.log('Silvana, você ainda não pode se aposentar!');
+    console .log('Silvana, você nao pode se aposentar!')
 }
